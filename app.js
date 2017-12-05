df1c68ae1bf9cee39f6cde03e09d6738902742f5
attax <westooy@gmail.com>
2017-12-05 23:58:05 +0800

    bugfix

--- app.js
+++ app.js
@@ -68,155 +68,78 @@ app.get(/^\/([A-Za-z0-9]{1,6})$/, (req, res) => {
 
         //ioreis支持promise
 
-        function queryURLRedisRecord(){
-        	return redis.hget('surl', surlId);
+        function queryURLRedisRecord() {
+            return redis.hget('surl', surlId);
         }
 
         //查询DB中url的记录
-        function queryURLDBRecord(){
-        	var SQL = 'SELECT target FROM `surl` WHERE uid=' + surlId;
-        	return new Promise(function(resolve, reject) {
-	            //执行查询
-	            conn.query(SQL, function(err, rows, fields) {
-	                if (err) {
-	                    console.log(err)
-                        reject(err); 
-	                } else {
-	                    console.log('enter', rows)
-	                    return resolve(rows);
-	                };
-	            });
+        function queryURLDBRecord() {
+            var SQL = 'SELECT target FROM `surl` WHERE uid=' + surlId;
+            return new Promise(function(resolve, reject) {
+                //执行查询
+                conn.query(SQL, function(err, rows, fields) {
+                    if (err) {
+                        console.log(err)
+                        reject(err);
+                    } else {
+                        console.log('enter', rows)
+                        return resolve(rows);
+                    };
+                });
 
-	        });
-        }
-        
-
-        function writeUnexistURLRedisRecord(){
-        	return  redis.hset('surl', surlId, 'nil');    
+            });
         }
 
-       
+
+        function writeUnexistURLRedisRecord() {
+            return redis.hset('surl', surlId, 'nil');
+        }
 
 
-        (async function(){
-        	var _redisResult=await queryURLRedisRecord();
-        	//redis中没有记录
-        	if(_redisResult==null){
 
-        		var _dbResult=await queryURLDBRecord();
+        (async function() {　
+            try {
 
-        		if (_dbResult.length) {
-                    var target = _dbResult[0].target;
-                    //重定向到相应链接
-                    res.redirect(301, target);
-                    res.end();
-                } else {
-                    //写入Redis缓存，设置默认值nil
-                    redis.hset('surl', surlId, 'nil')
-                        .then(function(result) {
-                            return Promise.resolve('设置Redis surl ' + surlId + '字段值为默认值nil成功');
-                        }, function(err) {
-                            return Promise.resolve({ 'msg': '设置Redis surl ' + surId + '字段值为默认值nil失败', 'Error': err });
-                        });
+                var _redisResult = await queryURLRedisRecord();
+                //redis中没有记录
+                if (_redisResult == null) {
 
-                    res.end('404');
+                    var _dbResult = await queryURLDBRecord();
+
+                    if (_dbResult.length) {
+                        var target = _dbResult[0].target;
+                        //重定向到相应链接
+                        res.redirect(301, target);
+                        res.end();
+                    } else {
+                        //写入Redis缓存，设置默认值nil
+                        redis.hset('surl', surlId, 'nil')
+                        res.end('404');
+                    }
+
+
                 }
+                /**
+                 * 如果返回了nil,说明此前查询过Redis和MySQL,均不存在该surlId字段，
+                 * 且设置该字段在Redis中为nil,则本次直接返回404
+                 */
+                else if (_redisResult == 'nil') {
+                    res.end('404');
+                } else {
+                    //redis中有记录，直接重定向到target页面
+                    res.redirect(301, _redisResult);
+                    res.end();
+                }
+                console.log(_redisResult)
+                console.log('redis output')
+            } catch (error) {
+                console.log('catching', error);
+            } finally {
+                console.log('finally')
+            }
 
 
-        	}
-        	/**
-             * 如果返回了nil,说明此前查询过Redis和MySQL,均不存在该surlId字段，
-             * 且设置该字段在Redis中为nil,则本次直接返回404
-             */   
-        	else if(_redisResult=='nil'){
-        		 res.end('404');
-    		}else{
-    			//redis中有记录，直接重定向到target页面
-		        res.redirect(301, target);
-		        res.end();
-    		}
-        	console.log(_reidsResult)
-        	console.log('redis output')
-        }());
-
-
-        // redis.hget('surl', surlId)
-        //     .then(function(result) {
-        //         /**
-        //          *  Redis中字段为surl的hash中surlId字段的值
-        //          */
-
-        //         if (result == null) {
-        //             //如果没有查询到，
-        //             return Promise.resolve(surlId);
-        //         }
-        //         /**
-        //          * *如果返回了nil,说明此前查询过Redis和MySQL,均不存在该surlId字段，
-        //          * 且设置该字段在Redis中为nil,则本次直接返回404
-        //          */
-        //         else if (result == 'nil') {
-        //             res.end('404');
-        //             return Promise.reject();
-        //         } else {
-        //             //有返回值，直接重定向到target页面
-        //             res.redirect(301, target);
-        //             res.end();
-        //             return Promise.reject();
-        //         }
-
-        //     }, function(err) {
-        //         console.log(err)
-        //     })
-        //     .then(function(surlId) {
-
-        //         return new Promise(function(resolve, reject) {
-        //             var SQL = 'SELECT target FROM `surl` WHERE uid=' + surlId;
-        //             //执行查询
-        //             conn.query(SQL, function(err, rows, fields) {
-        //                 if (err) {
-        //                     return reject(err);
-
-        //                 } else {
-        //                     console.log('enter', rows)
-
-        //                     return resolve(rows);
-        //                 };
-        //             });
-
-        //         });
-
-        //     }, function(err) {
-        //         console.log(err)
-        //     })
-        //     .then(function(rows) {
-        //         console.log('testing:', rows)
-        //             //如果mysql中存在相关数据
-        //         if (rows.length) {
-        //             var target = rows[0].target;
-        //             //重定向到相应链接
-        //             res.redirect(301, target);
-        //             res.end();
-        //         } else {
-        //             //写入Redis缓存，设置默认值nil
-        //             redis.hset('surl', surlId, 'nil')
-        //                 .then(function(result) {
-        //                     return Promise.resolve('设置Redis surl ' + surlId + '字段值为默认值nil成功');
-        //                 }, function(err) {
-        //                     return Promise.resolve({ 'msg': '设置Redis surl ' + surId + '字段值为默认值nil失败', 'Error': err });
-        //                 });
-
-        //             res.end('404');
-        //         }
-        //     }, function(err) {
-        //         console.log('debug', err)
-        //     })
-        //     .then(function(val) {
-        //         console.log(val)
-        //     }, function(err) {
-        //         console.log(err)
-        //     }).catch(function(err) {
-        //         console.log(err)
-        //     });
+        })();
 
 
     }
@@ -235,7 +158,6 @@ app.get('/addurl/', (req, res) => {
 
     var localSiteUrlReg = /^((http|https):\/\/)?localhost\/\w*/
     var urlReg = /(http | ftp | https)/;
-    console.log(queryURL)
     //如果添加的链接存在
     if (queryURL != undefined && queryURL != '') {
 
@@ -245,78 +167,94 @@ app.get('/addurl/', (req, res) => {
         var SQL = 'SELECT uid FROM `surl` WHERE target ="' + queryURL + '"';
 
         //查询db中是否存在url的记录
-        var queryURLRecord=function(){
-            
+        var queryURLRecord = function() {
+
             //console.log('unde', queryURL)
-            return new Promise(function(resolve,reject){
+            return new Promise(function(resolve, reject) {
                 conn.query(SQL, (err, rows, fields) => {
                     if (err) {
                         console.log(err)
-                        reject(err); 
+                        reject(err);
                     } else {
                         resolve(rows);
                     }
-                    
+
                 });
-           }) 
+            })
         };
 
         //插入到数据库中
         var InsertSQL = 'INSERT INTO `surl` (target) VALUES ("' + queryURL + '")';
 
-        var insertURLRecord=function(){
+        var insertURLRecord = function() {
 
-            return new Promise(function(resolve,reject){
+            return new Promise(function(resolve, reject) {
                 conn.query(InsertSQL, (err, rows, fields) => {
-                    if(err) {
+                    if (err) {
                         console.log(err)
-                        reject(err); 
+                        reject(err);
                     } else {
                         resolve(rows);
                     }
                 });
-               
+
             });
         };
-       
+
+
+        var insertURLRedisRecord = function(surlId, target) {
+            //写入Redis缓存，设置默认值nil
+            redis.hset('surl', surlId, target)
+        }
 
         //async执行
-        (async function(){
-            //db中的url记录
-            var urlRows=await queryURLRecord();
-            
-            if(urlRows.length){
-                var uid=urlRows[0].uid;
+        (async function() {
 
-                //返回的数据
-                result.code = 200;
-                result.url = queryURL;
-                result.surl = surl.idToURL(uid);
+            try {
+                //db中的url记录
+                var urlRows = await queryURLRecord();
 
-                //如果是jsonp
-                if (jsonpName != undefined) {
-                    res.end(jsonpName + '(' + JSON.stringify(result) + ')');
+                if (urlRows.length) {
+                    var uid = urlRows[0].uid;
+
+                    //返回的数据
+                    result.code = 200;
+                    result.url = queryURL;
+                    result.surl = surl.idToURL(uid);
+
+                    //如果是jsonp
+                    if (jsonpName != undefined) {
+                        res.end(jsonpName + '(' + JSON.stringify(result) + ')');
+                    } else {
+
+                        res.json(result);
+                    }
                 } else {
+                    var _insertResult = await insertURLRecord();
+                    var _insertRedisResult = await insertURLRedisRecord(_insertResult.insertId, queryURL)
+                    console.log('redis', _insertRedisResult)
+                        //返回的数据
+                    result.code = 200;
+                    result.url = queryURL;
+                    result.surl = surl.idToURL(_insertResult.insertId);
 
-                    res.json(result);
+                    console.log(result.surl)
+
+                    //如果是jsonp
+                    if (jsonpName != undefined) {
+                        res.end(jsonpName + '(' + JSON.stringify(result) + ')');
+                    } else {
+                        res.json(result);
+                    }
                 }
-            }else{
-               var _insertResult=await insertURLRecord();
 
-                //返回的数据
-                result.code = 200;
-                result.url = queryURL;
-                result.surl = surl.idToURL(_insertResult.insertId);
-
-                console.log(result.surl)
-
-                //如果是jsonp
-                if (jsonpName != undefined) {
-                    res.end(jsonpName + '(' + JSON.stringify(result) + ')');
-                } else {
-                    res.json(result);
-                }
+            } catch (error) {
+                console.log('catching', error);
+            } finally {
+                console.log('finally')
             }
+
+
 
         })();
 
